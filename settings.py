b73db7ad2941f9590f3e72d666ca3849a0ad4688
Krishna2005-m <kalikrishnakumarim@gmail.com>
2025-05-09 11:46:17 +0530

    settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
 pip install dj.database-url
+import dj.database-url
+import os
+
+DATABASE = {
+  "default":dj_database_url.parse(os.environ.get("postgresql://test_db_3g5k_user:CEVBDa5ghaeNExGmYWo4qqkNMzqfejnV@dpg-d0epg0h5pdvs73au8520-a.oregon-postgres.render.com/test_db_3g5k"))
